8de7c9368cf97710feb69d73aa8b712bd1e25651
Mariya Fareed <mariyafareed@Mariyas-Air.Dlink>
2021-05-27 21:30:58 +0530

    fesrefefs

--- config/database.js
+++ config/database.js
@@ -1,43 +1,26 @@
-const parse = require("pg-connection-string").parse;
+const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL);
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgres",
-            host,
-            port,
-            database,
-            username: user,
-            password,
-            ssl: { rejectUnauthorized: false },
-          },
-          options: {
-            ssl: false,
-          },
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: { rejectUnauthorized: false },
+        },
+        options: {
+          ssl: false,
         },
       },
-    };
-  } else {
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "sqlite",
-            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-          },
-          options: {
-            useNullAsDefault: true,
-          },
-        },
-      },
-    };
-  }
+    },
+  };
 };
